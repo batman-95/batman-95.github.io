8b503262a4a1015245d7f838d759218d9d997823
batman-95 <102166414+batman-95@users.noreply.github.com>
2022-03-26 20:34:39 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
    alert("Hello JavaScript");
-   let targetImage = document.querySelector("#smart-image");
+   
     targetImage.addEventListener("click", function() {
     ("点击了图片");
        targetImage.classList.remove("small");
